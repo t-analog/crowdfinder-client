1b25c5f13af6b4c213f13233e6bba32db28f46bc
psakurai <Oston.Martini@protonmail.com>
2021-11-30 23:16:42 +0800

    feat: add activity input screen
    refactor: improve activity screen design

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,129 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, View, Text, Button, StyleSheet, TouchableOpacity, TextInput,Image} from 'react-native';
 
 const HomeScreen= ({navigation}) => {
-return (
-  <View style={styles.container}> <Text>HomeScreen</Text>
-    <Button
-    title="Click Here"
-    onPress={() => alert('Button Clicked!')}
-    />
+  const [shouldShow, setshouldShow] = useState(true);
+  	return (
+  	<SafeAreaView style={{flex: 1}}>
+  <View style={styles.container}>
+  {
+  	shouldShow ? (
+      <View>
+        <TextInput style = {styles.input}
+           underlineColorAndroid = "transparent"
+           placeholder = "Event's Name"
+           placeholderTextColor = "black"
+           autoCapitalize = "none"/>
+
+         <TextInput style = {styles.input}
+           underlineColorAndroid = "transparent"
+           placeholder = "Description"
+           placeholderTextColor = "black"
+           autoCapitalize = "none"/>
+
+         <TextInput style = {styles.input}
+           underlineColorAndroid = "transparent"
+           placeholder = "Capacity"
+           placeholderTextColor = "black"
+           autoCapitalize = "none"/>
+
+         <TextInput style = {styles.input}
+           underlineColorAndroid = "transparent"
+           placeholder = "Category"
+           placeholderTextColor = "black"
+           autoCapitalize = "none"/>
+
+         <TextInput style = {styles.input}
+           underlineColorAndroid = "transparent"
+           placeholder = "Location"
+           placeholderTextColor
+           autoCapitalize = "none"/>
+
+         <View style={styles.row}>
+         <TouchableOpacity
+           style = {styles.buttonCancel}
+           onPress={() => setshouldShow(!shouldShow)}>
+           <Text style = {styles.ButtonText}> Cancel </Text>
+          </TouchableOpacity>
+
+         <TouchableOpacity
+           style = {styles.buttonCreate}
+           >
+           <Text style = {styles.ButtonText}> Create </Text>
+          </TouchableOpacity>
+         </View>
+      </View>
+  	) : null
+  }
+  <TouchableOpacity
+     style = {styles.buttonActivity}
+     onPress={() => setshouldShow(!shouldShow)}>
+     <Text style = {styles.ButtonText}> Create Activity </Text>
+  </TouchableOpacity>
   </View>
-  );
+  </SafeAreaView>
+  	);
 };
 
 export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
+     paddingTop: 100,
+     backgroundColor: 'white'
+  },
+  row: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#8fcbbc'
-},
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  logo: {
+     aspectRatio:1.81,
+     width: undefined,
+     justifyContent: 'center',
+     alignItems: 'center',
+     marginRight: 15,
+     marginLeft: 15,
+     resizeMode: 'contain'
+  },
+  input: {
+     paddingHorizontal: 10,
+     marginTop: 15,
+     marginRight: 15,
+     marginLeft: 15,
+     marginBottom: 0,
+     height: 40,
+     borderColor: 'black',
+     borderWidth: 1
+  },
+  buttonActivity: {
+     backgroundColor: 'black',
+     padding: 10,
+     margin: 15,
+     height: 40,
+     alignItems: 'center',
+     justifyContent: 'center',
+  },
+  buttonCancel: {
+     backgroundColor: 'black',
+     padding: 10,
+     paddingRight: 63,
+     paddingLeft: 63,
+     margin: 15,
+     height: 40,
+     alignItems: 'flex-start',
+     justifyContent: 'center',
+  },
+  buttonCreate: {
+     backgroundColor: 'black',
+     padding: 10,
+     paddingRight: 63,
+     paddingLeft: 63,
+     margin: 15,
+     height: 40,
+     alignItems: 'flex-end',
+     justifyContent: 'center',
+  },
+  ButtonText:{
+     color: 'white'
+  }
 });
