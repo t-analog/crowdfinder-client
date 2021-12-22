c6bb5651c53009119841d14187f9ba6c5dd8b99e
Afiq Nazrie <afnazrie@gmail.com>
2021-12-22 22:59:06 +0800

    feat: initial realm implementation

--- App.js
+++ App.js
@@ -1,3 +1,21 @@
 import LoginScreen from './screens/Login';
+import Realm from 'realm';
+
+const id = "crowdfinder-byjrt";
+const app = new Realm.App({ id });
+const credentials = Realm.Credentials.emailPassword(
+  "first@first.com",
+  "password"
+);
+
+const login = (async () => {
+  try {
+    const user = await app.logIn(credentials);
+    console.log("Successfully logged in!", user.id);
+    return user;
+  } catch (err) {
+    console.error("Failed to log in", err.message);
+  }
+})();
 
 export default LoginScreen;
