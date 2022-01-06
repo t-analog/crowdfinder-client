0324884dcb502ad40f6b383445e5aa3c83bb64df
arif amiruddin <76756035+psakurai@users.noreply.github.com>
2022-01-06 19:41:33 +0800

    style: update HomeScreen.js code style

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,27 +3,25 @@ import {View, Text, Image, Button, TextInput, TouchableOpacity, StyleSheet} from
 
 const ProfileScreen= ({navigation}) => {
 return (
-      <View style={styles.container}>
-      <Text style={styles.header}>Your Profile</Text>
-      <Image
-        style={styles.profile}
-        source={require('../assets/profile.jpeg')}
-      />
-      <Text style={styles.data}>@xijinping</Text>
-      <Text style={styles.data}>xi@cpc.cn</Text>
-      <Text style={styles.data}>Skudai</Text>
-      <TextInput secureTextEntry={false} multiline={true} style={styles.input}
-        underlineColorAndroid="transparent"
-        placeholder="President of the People's Republic of China & General Secretary of the Communist Party of China."
-        placeholderTextColor="black"
-        autoCapitalize="none"
-        editable={false}
-      />
-      <TouchableOpacity
-        style={styles.button}>
-        <Text style={styles.ButtonText}>Edit Profile</Text>
-      </TouchableOpacity>
-    </View>
+  <View style={styles.container}>
+    <Text style={styles.header}>Your Profile</Text>
+     <Image
+       style={styles.profile}
+       source={require('../assets/profile.jpeg')} />
+     <Text style={styles.data}>@xijinping</Text>
+     <Text style={styles.data}>xi@cpc.cn</Text>
+     <Text style={styles.data}>Skudai</Text>
+     <TextInput secureTextEntry={false} multiline={true} style={styles.input}
+       underlineColorAndroid="transparent"
+       placeholder="President of the People's Republic of China & General Secretary of the Communist Party of China."
+       placeholderTextColor="black"
+       autoCapitalize="none"
+       editable={false} />
+     <TouchableOpacity
+       style={styles.button}>
+       <Text style={styles.ButtonText}>Edit Profile</Text>
+     </TouchableOpacity>
+  </View>
   );
 };
 
