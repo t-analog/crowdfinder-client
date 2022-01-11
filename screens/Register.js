aa68cef8f26f7eeec82243509bd1480c030c70e5
Afiq Nazrie <afnazrie@gmail.com>
2022-01-12 00:52:51 +0800

    feat: initial user register implementation

--- screens/Register.js
+++ screens/Register.js
@@ -1,36 +1,57 @@
 import React from 'react';
+import { register } from '../utils/user';
 import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 
-const RegisterAccountScreen = () => {
+const RegisterAccountScreen = ({ navigation }) => {
+  const [username, onChangeUsername] = React.useState("");
+  const [password, onChangePassword] = React.useState("");
+  const [confirmPassword, onChangeConfirmPassword] = React.useState("");
   return (
     <View style={styles.container}>
-      <TextInput style={styles.input}
-        underlineColorAndroid="transparent"
-        placeholder="Username"
-        placeholderTextColor="black"
+      <Text>
+        Value of username: {username}{"\n"}
+        Value of password: {password}{"\n"}
+        Value of confirmPassword: {confirmPassword}
+      </Text>
+      <TextInput
+        style={styles.input}
         autoCapitalize="none"
-      />
-      <TextInput style={styles.input}
-        underlineColorAndroid="transparent"
-        placeholder="Email"
+        onChangeText={onChangeUsername}
+        placeholder="Email/Username"
         placeholderTextColor="black"
-        autoCapitalize="none"
-      />
-      <TextInput secureTextEntry={true} style={styles.input}
         underlineColorAndroid="transparent"
+      />
+      <TextInput
+        autoCapitalize="none"
+        onChangeText={onChangePassword}
         placeholder="Password"
         placeholderTextColor="black"
-        autoCapitalize="none"
-      />
-      <TextInput secureTextEntry={true} style={styles.input}
+        secureTextEntry={true}
+        style={styles.input}
         underlineColorAndroid="transparent"
+      />
+      <TextInput
+        autoCapitalize="none"
+        onChangeText={onChangeConfirmPassword}
         placeholder="Confirm Password"
         placeholderTextColor="black"
-        autoCapitalize="none"
+        secureTextEntry={true}
+        style={styles.input}
+        underlineColorAndroid="transparent"
       />
       <TouchableOpacity
-        style={styles.registerButton}>
-        <Text style={styles.ButtonText}>Register Account</Text>
+        style={styles.registerButton}
+        onPress={
+          async () => {
+            if (password != confirmPassword) {
+              console.error("Password and Confirm Password don't match");
+              return;
+            }
+            const user = await register({ username, password });
+            { user != 1 ? navigation.navigate("Home") : console.error("Failed to register") };
+          }
+        }>
+        <Text style={styles.ButtonText}>Register</Text>
       </TouchableOpacity>
     </View>
   );
