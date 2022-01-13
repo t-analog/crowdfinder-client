96ed2482aa7d03863ce4af0552cf0f63b39b3f81
Afiq Nazrie <afnazrie@gmail.com>
2022-01-13 18:54:41 +0800

    chore: use react-native-dotenv

--- utils/realm.js
+++ utils/realm.js
@@ -1,6 +1,7 @@
 import Realm from 'realm';
+import { REALM_APP_ID } from '@env';
 
-const id = "crowdfinder-byjrt";
+const id = `${REALM_APP_ID}`;
 const app = new Realm.App({ id });
 
 export { id, app };
