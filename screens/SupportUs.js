8eca91366e53a40432fa45ee0c53e0a1be7657d5
psakurai <Oston.Martini@protonmail.com>
2022-01-15 01:10:54 +0800

    feat: add drawer navigation

---
+++ screens/SupportUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+const SupportUs = () => {
+return (
+	<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+	  <Text>Support Us</Text>
+	</View>
+);
+};
+
+export default SupportUs;
