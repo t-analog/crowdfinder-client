4f3c7215b0351a3e818220c7fd4eb8d928542395
psakurai <Oston.Martini@protonmail.com>
2022-01-15 01:33:15 +0800

    feat: add logout screen

---
+++ screens/Logout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+const Logout = () => {
+return (
+	<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+	  <Text>Logout</Text>
+	</View>
+);
+};
+
+export default Logout;
