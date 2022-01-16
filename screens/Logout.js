357d59b3e8c8ede62eb2c30ee100f6a5fd892793
psakurai <Oston.Martini@protonmail.com>
2022-01-17 00:07:20 +0800

    refactor: move code to screens/Logout.js

--- screens/Logout.js
+++ screens/Logout.js
@@ -1,12 +1,35 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet
+} from 'react-native';
+import { logout } from '../utils/user';
 
-const Logout = () => {
+const Logout = ({ navigation }) => {
 return (
-	<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-	  <Text>Logout</Text>
+	<View style={styles.container}>
+		<Text>ProfileScreen</Text>
+		<Button
+			title="Logout"
+			onPress={
+				async () => {
+					if (await logout()) navigation.navigate("Login");
+				}
+			}
+		/>
 	</View>
 );
 };
 
 export default Logout;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#8fcbbc'
+  },
+});
