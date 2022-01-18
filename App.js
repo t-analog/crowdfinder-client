e3737937bb2d36e697520672ec0fedcd02ff315a
Afiq Nazrie <afnazrie@gmail.com>
2022-01-18 10:58:44 +0800

    chore: import core-js in project root

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import LoginStack from './components/LoginStack';
 import { ApolloProvider } from '@apollo/client';
 import client from './utils/graphql';
+import 'core-js/stable';
 
 const App = () => {
   return (
