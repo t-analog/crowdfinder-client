7eaa6875037230919ac219296ac2302f40278101
Afiq Nazrie <afnazrie@gmail.com>
2022-01-19 20:50:54 +0800

    feat: profile screen menu

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,41 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
   Button,
   StyleSheet
 } from 'react-native';
+import {
+  Menu,
+} from 'react-native-paper';
 import { logout } from '../utils/user';
-import { createActivity, getActivities } from '../utils/activity';
 
 const ProfileScreen = ({ navigation }) => {
+  const [menuVisible, setMenuVisible] = useState(false);
+  const openMenu = () => setMenuVisible(true);
+  const closeMenu = () => setMenuVisible(false);
+
   return (
     <View style={styles.container}>
       <Text>ProfileScreen</Text>
-      <Button
-        title="Logout"
-        onPress={
-          async () => {
-            if (await logout()) navigation.navigate("Login");
+      <Menu
+        visible={menuVisible}
+        onDismiss={closeMenu}
+        /* change this anchor component into triple-dot + style */
+        anchor={<Button
+                  title="View Menu"
+                  onPress={openMenu}
+                />}>
+        <Menu.Item
+          title="Activity Settings"
+          onPress={() => {
+            alert("Activity Settings clicked");
+          }}
+        />
+        <Menu.Item
+          title="Support Us"
+          onPress={() => {
+            alert("Support Us clicked");
+          }}
+        />
+        <Menu.Item
+          title="Logout"
+          onPress={async () => {
+              if (await logout()) navigation.navigate("Login");
+            }
           }
-        }
-      />
-      <Button
-        title="GetActivities"
-        onPress={
-          async () => {
-            console.log(await getActivities())
-          }
-        }
-      />
-      <Button
-        title="CreateActivity"
-        onPress={
-          async () => {
-            console.log(await createActivity())
-          }
-        }
-      />
+        />
+      </Menu>
     </View>
   );
 };
