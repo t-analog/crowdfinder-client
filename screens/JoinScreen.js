cb889cee58aa29720cb244f539a707835d336992
nazreen <nazreenmubin00@gmail.com>
2022-01-20 16:39:54 +0800

    perf: update joinscreen with drawer - v2

--- screens/JoinScreen.js
+++ screens/JoinScreen.js
@@ -1,24 +1,52 @@
 import * as React from 'react';
 import Activity from '../components/Activity';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
 import * as activityExample from '../examples/activityExample.json'
+import BottomDrawer from 'react-native-bottom-drawer-view';
+
+
+const windowHeight = 4 / 5 * Dimensions.get('window');
+var isHidden = true;
 
 const JoinScreen = ({ navigation }) => {
   return (
-    <View style={styles.activityWrapper}>
-      <Text style={styles.sectionTitle}>Activity Nearby</Text>
-      <ScrollView style={styles.items}>
-        {activityExample.activities.map((activity, id) => (
-          <Activity
-            key={id}
-            title={activity.title}
-            description={activity.description}
-            location={activity.location}
-            categories={activity.categories}
-          />
-        ))}
-      </ScrollView>
-    </View>
+    <BottomDrawer
+      containerHeight={600}
+      offset={10}
+      downDisplay={415}
+      roundedEdges={true}
+    >
+        <View style={styles.activityWrapper}>
+
+        <View style={{
+            width: 40,
+            borderColor: 'black',
+            borderWidth: 1,
+            borderRadius: 5,
+            alignSelf: 'center',
+          }}  
+        />
+        <Text style={styles.sectionTitle}>
+          Activity Nearby
+        </Text>
+        
+        <ScrollView style={styles.items}>
+          
+          <TouchableOpacity>
+            {activityExample.activities.map((activity, id) => (
+                        <Activity
+                          key={id}
+                          title={activity.title}
+                          description={activity.description}
+                          location={activity.location}
+                          categories={activity.categories}
+                        />
+            ))}
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+      
+    </BottomDrawer>
   );
 }
 
@@ -26,19 +54,21 @@ const styles = StyleSheet.create({
   activityWrapper: {
     flex: 1,
     backgroundColor: '#EBEAED',
-    paddingTop: 20,
+    paddingTop: 10,
     paddingHorizontal: 20,
-    marginTop: '33%',
+    // marginTop: '33%',
     borderRadius: 20,
     paddingBottom: 40,
+    bottom: 0,
+    marginBottom: 120
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: 'bold',
-    paddingTop: 10,
+    paddingVertical: 20,
   },
   items: {
-    marginTop: 20
+    marginTop: 20,
   },
 });
 
