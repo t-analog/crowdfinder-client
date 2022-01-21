23a38c7bc389eff11d5250e76ab8c47bdbb67012
psakurai <Oston.Martini@protonmail.com>
2022-01-21 21:55:50 +0800

    refactor: move profile screen stylesheet to styles/stylesheet

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -11,6 +11,7 @@ import {
   Menu, Appbar
 } from 'react-native-paper';
 import { logout } from '../utils/user';
+import styles from '../styles/stylesheet';
 
 const ProfileScreen = ({ navigation }) => {
   const [edit, setEdit] = useState(false);
@@ -122,104 +123,3 @@ const ProfileScreen = ({ navigation }) => {
 };
 
 export default ProfileScreen;
-const styles = StyleSheet.create({
-  rightmenu: {
-    top: 19,
-    left: 258
-  },
-  color: {
-    backgroundColor: 'white',
-  },
-  container: {
-    paddingTop: 100,
-    backgroundColor: 'white'
-  },
-  header: {
-    marginTop: 20,
-    marginBottom: 5,
-    marginLeft: 15,
-    fontSize: 40
-  },
-  data: {
-    fontSize: 16
-  },
-  profile: {
-    width: 120,
-    height: 120,
-    borderRadius: 120 / 2,
-    position: 'absolute',
-    right: 10,
-    top: 5
-  },
-  box: {
-    paddingHorizontal: 8,
-    paddingVertical: 10,
-    marginTop: 15,
-    marginRight: 15,
-    marginLeft: 15,
-    marginBottom: 0,
-    borderWidth: 1,
-    borderColor: "black",
-    borderRadius: 5,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  boxA: {
-    paddingHorizontal: 8,
-    paddingVertical: 10,
-    marginTop: 15,
-    marginRight: 15,
-    marginLeft: 15,
-    marginBottom: 0,
-    borderWidth: 1,
-    borderColor: "black",
-    borderRadius: 5,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  boxB: {
-    marginTop: -5,
-    justifyContent: 'space-between',
-    color: "black",
-    fontSize: 16
-  },
-  boxB2: {
-    marginTop: -5,
-    marginRight: 9,
-    paddingTop: 28,
-    justifyContent: 'space-between',
-    color: "black",
-    fontSize: 16
-  },
-  button: {
-    padding: 10,
-    marginTop: 30,
-    marginLeft: 15,
-    marginRight: 15,
-    height: 40,
-    backgroundColor: 'black',
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  buttonSplit: {
-    padding: 10,
-    marginTop: 30,
-    marginLeft: 15,
-    marginRight: 15,
-    height: 40,
-    backgroundColor: 'black',
-    borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width:176,
-    justifyContent: 'space-between'
-  },
-  ButtonText: {
-    color: 'white',
-    fontSize: 16
-  },
-  row: {
-    flexDirection: 'row',
-  }
-});
