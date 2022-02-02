2082a743e8fbecf1bbb14d7757f643b814756327
Wan Muhammad Fikri <wanmuhammadfikri18@gmail.com>
2022-02-02 15:42:57 +0800

    refactor: fix data validation for capacity and remove data validation for location

--- screens/CreateActivity.js
+++ screens/CreateActivity.js
@@ -179,12 +179,6 @@ const CreateActivity = () => {
             </Pressable>
           </View>
         </View>
-        
-        <View
-              style={{
-                height: 150,
-              }}
-            />
       </BottomDrawer>
     </View>
   );
